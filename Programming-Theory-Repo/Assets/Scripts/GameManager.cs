484630b8345638a415837c5c2d439a0423a41d0d
Julio <julio.c.sanchez.pro@gmail.com>
2022-06-01 15:39:54 -0700

    added scene management

--- Programming-Theory-Repo/Assets/Scripts/GameManager.cs
+++ Programming-Theory-Repo/Assets/Scripts/GameManager.cs
@@ -1,21 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private Player player;
     // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.Find("Player").GetComponent<Player>();
+    public void GameOver() {
+        SceneManager.LoadScene("titleScene");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(player.PlayerHealth <= 0){
-            Debug.Log("GAME OVER!");
-        }
+    public void Play() {
+        SceneManager.LoadScene("SampleScene");
     }
 }
--- Programming-Theory-Repo/Assets/Scripts/Player.cs
+++ Programming-Theory-Repo/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private GameManager gameManager;
+
     [SerializeField]
     private int playerhealth;
     public int PlayerHealth
@@ -14,10 +16,24 @@ public class Player : MonoBehaviour
                 playerhealth = 0;
         }
     }
+
     public void Damage(int dmgValue)
     {
         playerhealth -= dmgValue;
         Debug.Log($"PLayer Damaged -- Player's health: {playerhealth}");
 
     }
+
+    private void Start() 
+    {
+        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    private void Update() {
+      
+      if(playerhealth <= 0) {
+          gameManager.GameOver();
+      }
+  }
 }
